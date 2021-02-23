84d84964508816f08b3f46e4b08cce670a110723
armit <armit@LAPTOP-EUSQQV7M>
2021-02-23 17:00:20 -0500

    test

--- proj3/src/application/Main.java
+++ proj3/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
-        primaryStage.setTitle("Payroll Processing System");
+        primaryStage.setTitle("Payroll Processing");
         primaryStage.setScene(new Scene(root, 400, 300));
         primaryStage.show();
     }
